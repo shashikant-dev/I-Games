2cd9d8b715df12007a1c472142c7c9be5e044080
Shashikant <developer.shashikant@gmail.com>
2025-07-13 01:07:48 +0530

    Update .gitignore to include .next directory, modify app-paths-manifest.json to add favicon route, and update webpack-runtime.js for compatibility. Remove unused not-found component from vendor-chunks. Clean up cache by deleting obsolete image file.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("fa4a5b5c522a03a3")
+/******/ 		__webpack_require__.h = () => ("e3014d7b4bd2bab4")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
